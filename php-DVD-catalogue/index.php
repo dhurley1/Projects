fd14e8c4c14dee9dc6972c567ade6d0474142149
David Hurley <david.j.hurley@mycit.ie>
2014-09-26 01:53:58 +0100

    Update index.php

--- php-DVD-catalogue/index.php
+++ php-DVD-catalogue/index.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <head>
-<title>
-DVD Catalogue</title>
-<meta charset="utf-8"/>
-
+   <title>Enter a New DVD</title>
+   <meta charset="utf-8"/>
+   <link rel="stylesheet" href="index.CSS" type="text/css" charset="utf-8">
 </head>
 <body>
-<?php
-
-?>
-
-<a href="database.php">view a list of the currently available DVD titles</a>
-
+   <h1>Online DVD Catalogue</h1>
+   <div id="content">
+      </br>
+      <a href="database.php">view a list of the currently available DVD titles</a>
+      </br></br></br></br>
+      <?php
+         include("cookieCounter.php");
+         
+         include("cookieDate.php");
+         
+         
+            ?>
+      </br>
+   </div>
 </body>
 </html>
-
-
-
-
