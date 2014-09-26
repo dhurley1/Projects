e3fa3582c38d8a8228dcedd551362860c7c8247c
David Hurley <david.j.hurley@mycit.ie>
2014-09-26 01:38:46 +0100

    Create inndex.php

---
+++ php-DVD-catalogue/inndex.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<title>
+DVD Catalogue</title>
+<meta charset="utf-8"/>
+
+</head>
+<body>
+<?php
+
+?>
+
+<a href="database.php">view a list of the currently available DVD titles</a>
+
+</body>
+</html>
+
+
+
+
