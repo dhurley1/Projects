18da69b92e513148a0178032fc6a99bd355a2746
David Hurley <david.j.hurley@mycit.ie>
2014-09-26 01:57:26 +0100

    Update delete.php

--- php-DVD-catalogue/delete.php
+++ php-DVD-catalogue/delete.php
@@ -1,79 +1,41 @@
 <!DOCTYPE html>
 <head>
-<title>Update DVD</title>
-<meta charset="utf-8"/>
-<link rel="stylesheet" href="Files.CSS" type="text/css"/ charset="utf-8">
+   <title>
+      currently available DVD's 
+   </title>
+   <meta charset="utf-8"/>
+   <link rel="stylesheet" type="text/css" href="index.css" />
 </head>
 <body>
-<?php
-if(isset($_POST['delete']))
-{
-$conn = mysqli_connect("157.190.64.130","R00068614","ub96BZ");
-if(!$conn)
-{
-$error="<p>Unable to connect to the database server.</p>\n "
-         ."<p>Connection error code ".mysqli_connect_errno()."</p> \n";
-		 
-		 include 'error.html.php';
-		 exit();
-}
-
-
-$id =$_POST['ID'];
-$TableName="titles";
-
-$sql="DELETE FROM $TableName WHERE ID =$id ";
-
-if (!mysqli_query($conn, $sql))
-						{
-						
-							echo "<p>There was an error deleting the record.<br/>\n" .
-								 "The error was " . 
-								 htmlspecialchars(mysqli_error($conn), ENT_QUOTES) .
-								 ".<br/>\nThe query was '" .
-								 htmlspecialchars($sql,ENT_QUOTES) .
-								 "'</p>\n";
-								 
-						}
-
-		
-		if (!mysqli_select_db($conn, $DBName))
-		{
-		
-			$error = "<p>Unable to connect to the $DBName database!</p>\n"
-				. "<p>Error code " . mysqli_errno($conn)
-					. ": " . mysqli($conn) . "</p>\n";
-					
-					
-			include 'error.html.php';
-			exit();		
-		
-		}
-echo "deleted data successfully";
-mysqli_close($conn);
-}
-else
-{
-?>
-<form method="post" action="<?php $_PHP_SELF ?>">
-<table width="400" border="0" cellspacing="1" cellpadding="2">
-<td width="100">Movie ID</td>
-<td><input name="ID" type="text" id="ID"></td>
-</tr>
-<tr>
-<td width="100"> </td>
-<td> </td>
-</tr>
-<tr>
-<td width="100"> </td>
-<td>
-<input name="delete" type="submit" id="delete" value="Delete">
-</td>
-</tr>
-</table>
-</form>
-<?php
-}
-?>
+   <h1>DVD online catalogue</h1>
+   <div id="content">
+   <?php
+         //database connection   
+     // $DBConnection = mysql_connect("157.190.64.130",  "R00068614" , "ub96BZ");
+     $DBConnection = mysql_connect('127.0.0.1','root');
+       if(!$DBConnection)
+         {
+         $error="<p>Unable to connect to the database server.</p>\n "
+                  ."<p>Connection error code ".mysqli_connect_errno()."</p> \n";
+         		 
+         		 include 'error.html.php';
+         		 exit();
+         }
+		 $DBName = "r00068614dvds";
+      mysql_select_db($DBName, $DBConnection);
+      
+	  
+      $id =$_REQUEST['ID'];
+      $table = "titles";
+      
+      // query
+      mysql_query("DELETE FROM $table WHERE ID = '$id'")
+      or die(mysql_error()); 
+       
+	   echo "<p>dvd deleted</p>";
+	  
+      ?>
+   <a href="database.php">View DVDs</a>
+   </div>
 </body>
 </html>
